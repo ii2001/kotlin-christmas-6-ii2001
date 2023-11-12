bb4cec172a8f56632f6bd04f2b0e63828ce9b14e
ii2001 <iicks2001@gmail.com>
2023-11-12 16:15:01 +0900

    feat: EventResult에 필요할 것 같은 데이터값 만들기

--- src/main/kotlin/christmas/EventResult.kt
+++ src/main/kotlin/christmas/EventResult.kt
@@ -1,2 +1,11 @@
 package christmas
 
+data class EventResult(
+    val orderMenu: List<String>,
+    val totalBeforeDiscount: Int,
+    val giftMenu: String,
+    val discountAmount: Int,
+    val totalBenefits: Int,
+    val totalAfterDiscount: Int,
+    val eventBadge: String
+)
