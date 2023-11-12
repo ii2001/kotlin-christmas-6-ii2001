d8b6442de738aba68007bbb0d01e61341824e052
ii2001 <iicks2001@gmail.com>
2023-11-12 15:57:26 +0900

    feat: PlannerModel파일 함수 리스트 만들기

--- src/main/kotlin/christmas/PlannerModel.kt
+++ src/main/kotlin/christmas/PlannerModel.kt
@@ -1,2 +1,18 @@
 package christmas
 
+class PlannerView {
+    fun showWelcomeMessage() {
+        // 환영 메시지 출력
+    }
+
+    fun getVisitDate(): Int {
+        // 날짜 입력 받기
+    }
+
+    fun getOrderItems(): Map<String, Int> {
+        // 주문 메뉴 및 수량 입력 받기
+    }
+    fun showError(errorMessage: String?) {
+        // 에러 메시지 출력
+    }
+}
